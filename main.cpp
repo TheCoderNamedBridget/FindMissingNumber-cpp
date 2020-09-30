8b03f48e9e3d94e44dc84fc1e45beb56a97242fd
Bridget Naylor <48873843+TheCoderNamedBridget@users.noreply.github.com>
2020-09-30 13:02:27 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,134 @@ Bridget Naylo
 Lab 2 
 *******************************************************************************/
 #include <stdio.h>
+#include <iostream>/******************************************************************************
+
+Bridget Naylor
+Lab 2 
+*******************************************************************************/
+#include <stdio.h>
 #include <iostream>
+using namespace std;
+
+int findSQRT( int num )
+{
+    bool squareFound= false;
+    int firstSquare = 0;
+    int lastSquare = num / 2;
+    int middleSquare = (firstSquare + lastSquare) / 2;
+    int middleSquarePlusOne = middleSquare + 1;
+    
+    while ( !squareFound )
+    {
+        if ( num == middleSquare * middleSquare )
+        {
+            return middleSquare;
+        }
+        else if ( num > middleSquare * middleSquare && num <= middleSquarePlusOne * middleSquarePlusOne )
+        {
+            return middleSquarePlusOne;
+        }
+        else if ( num > middleSquare * middleSquare )
+        {
+            firstSquare = middleSquare;
+            //int lastSquare = lastSquare;
+            
+            middleSquare += (lastSquare - firstSquare);
+            middleSquarePlusOne = middleSquare + 1;
+        }
+        else if ( num < middleSquare * middleSquare )
+        {
+            //int firstSquare = firstSquare;
+            lastSquare = middleSquare;
+            
+            middleSquare = (firstSquare + lastSquare) / 2;
+            middleSquarePlusOne = middleSquare + 1;
+        }
+    }
+    
+    return 0;
+    
+}
+
+
+int findSmallestMissingNum( int n, int nums[] )
+{
+    bool found = false;
+    int start = 0;
+    int end = n;
+    int middle = n / 2;
+    while ( !found )
+    {
+        if ( middle == 0 || (nums[middle] != middle && nums[ middle - 1 ] == middle - 1 && middle - 1 >= 0))// 
+        {
+            found = true;
+            return middle;
+        }
+        else if ( nums[middle] == middle )
+        {
+            start = middle;
+            middle += (end - middle);
+        }
+        else if ( nums[middle] != middle )
+        {
+            end = middle;
+            middle = (middle + start)/2;
+        }
+        
+    }
+    return 0;
+}
+
+int main()
+{
+    //Part 1
+    cout<<"Part 1: Enter an integer that you want the square of: "<<endl;
+    int N;
+    cin>>N;
+    cout<<findSQRT( N );
+    
+    
+    //Part 2
+    cout<<"Part 2: Enter size of list: "<<endl;
+    int n;
+    cin>>n;
+    
+    int missingNumChance;
+    
+    int missingNumIndex = 0;
+    
+
+    
+    srand (time(0));
+    int listOfNums[n];
+    int value = 0;
+    for ( int i = 0; i < n ; i++ )
+    {
+        
+        //missingNumIndex = rand() % n ;
+        //cout<<"missingNumIndex "<<missingNumIndex<<endl;
+        missingNumChance = rand() % 10 ;
+        if ( missingNumChance > 7 )//30% chance this i will be a missing number
+        {
+            value ++;
+            listOfNums[i] = value;
+            //cout<<"missingNumChance "<<missingNumChance<<"missing num "<<i<<endl;
+        }
+        else 
+        {
+            listOfNums[i] = value;
+            //missingNumIndex = rand() % n ;
+        }
+        
+        cout<<"i = "<<i<<" item[i] = "<<listOfNums[i]<<endl;
+        value ++;
+    }
+    cout<<"Missing value = "<<findSmallestMissingNum( n, listOfNums );
+
+    return 0;
+}
+
+
 using namespace std;
 
 // int findSQRT( int num )
